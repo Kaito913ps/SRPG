bd13649af5a077055b86af9802fa3c4be25afc1e
DESKTOP-TRSHQ94\kaito <kaito913jp@gmail.com>
2023-03-04 11:27:00 +0900

    CameraZoomを修正

--- Assets/Scripts/CameraZoom.cs
+++ Assets/Scripts/CameraZoom.cs
@@ -2,30 +2,35 @@ using UnityEngine;
 
 public class CameraZoom : MonoBehaviour
 {
+    // メインカメラ
+    private Camera _mainCamera;
 
-	private Camera _mainCamera;
-
-	// 定数定義
-	const float ZOOM_SPEED = 0.1f; 
-	const float ZOOM_MIN = 40.0f; 
-	const float ZOOM_MAX = 60.0f; 
+    // 定数定義
+    // ズーム速度
+    const float ZOOM_SPEED = 0.1f;
+    // カメラの最小の視野
+    const float ZOOM_MIN = 40.0f;
+    // カメラの最大の視野
+    const float ZOOM_MAX = 60.0f; 
 
 	void Start()
 	{
-		_mainCamera = GetComponent<Camera>(); 
+        // カメラの参照を取得
+        _mainCamera = GetComponent<Camera>(); 
 	}
 
 	void Update()
 	{
-		if (Input.touchCount != 2)
+        // マルチタッチ(２点同時タッチ)でないなら終了
+        if (Input.touchCount != 2)
 			return;
 
 		// ２点のタッチ情報を取得する
 		var touchData_0 = Input.GetTouch(0);
 		var touchData_1 = Input.GetTouch(1);
 
-
-		float oldTouchDistance = Vector2.Distance( 
+        // １フレーム前の２点間の距離を求める
+        float oldTouchDistance = Vector2.Distance( 
 			touchData_0.position - touchData_0.deltaPosition, 
 			touchData_1.position - touchData_1.deltaPosition
 			);
